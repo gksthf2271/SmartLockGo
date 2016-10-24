c5446f3e921b79d5c48430c7379c844c95379b20
ESENS <ghdldid12@gmail.com>
2016-10-25 01:22:22 +0900

    이승준

--- app/src/main/java/com/eum/ssrgo/MainActivity.java
+++ app/src/main/java/com/eum/ssrgo/MainActivity.java
@@ -120,6 +120,7 @@ public class MainActivity extends AppCompatActivity
         //주석 추가
 //ㅁㄴㅇㅁㄴㅇㅁㄴㅇ
         //주석 또 추가
+        ///asdasdadsdasda
         navHeaderView = navigationView.inflateHeaderView(R.layout.nav_header_main);
         tv_username = (TextView)navHeaderView.findViewById(R.id.tv_UserName);
         tv_useremail = (TextView)navHeaderView.findViewById(R.id.tv_userEmail);
